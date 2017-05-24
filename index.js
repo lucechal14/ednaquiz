4e445b315875c954a13830658f3c6e4eef52c981
Alan Zambrano <alnzam17@gmail.com>
2017-05-24 17:43:18 -0500

    Included an example for downloading a file

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ app.get('/test', function (request, response) {
   response.render('pages/testingPage');
 });
 
+app.get('/download', function (request, response){
+  var file = __dirname + '/examples/typeform_webhook_response.json';
+  response.download(file);
+});
+
 app.listen(app.get('port'), function () {
   console.log('Example app listening on port ', app.get('port'));
 });
