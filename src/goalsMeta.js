0567d65e8817e75f4fd8b05ad6b1168717f45a63
Alan Zambrano <c-azambrano@lendingclub.com>
2017-05-29 10:19:28 -0500

    Adds goals meta

---
+++ src/goalsMeta.js
@@ -0,0 +1,69 @@
+module.exports = [{
+    goalClass: 'goal1',
+    goalName: 'Fin de la pobreza',
+    practices: []
+}, {
+    goalClass: 'goal2',
+    goalName: 'Hambre cero',
+    practices: ["practice1"] 
+}, {
+    goalClass: 'goal3',
+    goalName: 'Salud y bienestar',
+    practices: ["practice2"] 
+}, {
+    goalClass: 'goal4',
+    goalName: 'Educación de calidad',
+    practices: ["practice7"] 
+}, {
+    goalClass: 'goal5',
+    goalName: 'Igualdad de género',
+    practices: ["practice9"] 
+}, {
+    goalClass: 'goal6',
+    goalName: 'Agua limpia y saneamiento',
+    practices: ["practice14"] 
+}, {
+    goalClass: 'goal7',
+    goalName: 'Energía asequible y no contaminante',
+    practices: ["practice22"] 
+}, {
+    goalClass: 'goal8',
+    goalName: 'Trabajo decente y crecimiento económico',
+    practices: ["practice25"] 
+}, {
+    goalClass: 'goal9',
+    goalName: 'Industria, innovación e infraestructura',
+    practices: ["practice32"] 
+}, {
+    goalClass: 'goal10',
+    goalName: 'Reducción de las desigualdades',
+    practices: ["practice36"] 
+}, {
+    goalClass: 'goal11',
+    goalName: 'Ciudades y comunidades sostenibles',
+    practices: ["practice39"] 
+}, {
+    goalClass: 'goal12',
+    goalName: 'Producciíon y consumo responsables',
+    practices: ["practice46"] 
+}, {
+    goalClass: 'goal13',
+    goalName: 'Acción por el clima',
+    practices: ["practice54"] 
+}, {
+    goalClass: 'goal14',
+    goalName: 'Vida submarina',
+    practices: ["practice57"] 
+}, {
+    goalClass: 'goal15',
+    goalName: 'Vida de ecosistemas terrestres',
+    practices: ["practice59"] 
+}, {
+    goalClass: 'goal16',
+    goalName: 'Paz, justicia e instituciones sólidas',
+    practices: ["practice65"] 
+}, {
+    goalClass: 'goal17',
+    goalName: 'Alianzas para lograr los objetivos',
+    practices: ["practice67"]
+}];
--- src/transformViewModel.js
+++ src/transformViewModel.js
@@ -1,22 +1,33 @@
 var _ = require('lodash');
+var goals = require('./goalsMeta');
 
+var getGoalInfoByPractice = function(practiceId) {
+    return _.find(goals, function(g) {
+        return _.findIndex(g.practices, function(p) {
+            return p === practiceId;
+        }) != -1;
+    });
+};
 
-var questionsAsStringArray = function(questionArray) {
-    return _.reduce(questionArray, function(result, value, key){
-            if(!result.questions) {
-                result.questions = []; 
-            }
-            result.goalImageClass = "goal1";
-            result.goalName = "FIN DE LA POBREZA";
-            result.questions.push(value.title);
-            return result;
-        }, {});
+var questionsAsStringArray = function (questionArray) {
+    return _.reduce(questionArray, function (result, value, key) {
+        if (!result.questions) {
+            result.questions = [];
+        }
+
+        var goal = getGoalInfoByPractice(value.practice);
+
+        result.goalImageClass = goal.goalClass;
+        result.goalName = goal.goalName;
+        result.questions.push(value.title);
+        return result;
+    }, {});
 };
 
 
-module.exports = function(results) {
+module.exports = function (results) {
     var viewModel = results;
-    
+
     viewModel.categoryYesWithCertification = questionsAsStringArray(viewModel.categoryYesWithCertification);
     viewModel.categoryYesWithSelfAppraisal = questionsAsStringArray(viewModel.categoryYesWithSelfAppraisal);
     viewModel.categoryNo = questionsAsStringArray(viewModel.categoryNo);
