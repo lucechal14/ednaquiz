0e3f0d732dea4c00b6e108942d79c61601be8ba6
Alan Zambrano <c-azambrano@lendingclub.com>
2017-06-07 11:55:22 -0500

    Added deployment script via CircleCI - Heroku

--- test/transformation.test.js
+++ test/transformation.test.js
@@ -3,14 +3,12 @@ var jsonLoader = require('../src/jsonLoader');
 var Report = require('../src/report');
 var transform = require('../src/transformViewModel');
 
-describe('Transformation', function () {
+describe('Simple Transformation', function () {
 
     it('transforms results into view model', function () {
 
         var results = jsonLoader('herokuDbEntry.json');
     
         var viewModel = transform(results);
-
-        console.log(viewModel);
     });
 });
